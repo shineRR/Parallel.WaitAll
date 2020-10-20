7bb97886bea2415c55d3c0c1f55682d908ae2ae2
Ilya Baryko <38408304+shineRR@users.noreply.github.com>
2020-10-20 23:29:37 +0300

    Added project with log

---
+++ LogParallel/LogParallel/Program.cs
@@ -0,0 +1,16 @@
+﻿using LogParallel.Log;
+
+namespace LogParallel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LogBuffer logBuffer = new LogBuffer(32);
+            for (int i = 0; i < 10; ++i)
+            {
+                logBuffer.Add("LOGGGING");
+            }
+        }
+    }
+}
